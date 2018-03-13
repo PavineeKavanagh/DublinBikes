ca07c497f69d1de1d57e491056146e41bb8c006b
Harsh Gupta <harshrp94@gmail.com>
2018-03-13 21:48:05 +0000

    Correcting data type errors

--- dublinbikes/owm_scrapper.py
+++ dublinbikes/owm_scrapper.py
@@ -17,15 +17,20 @@ def main():
     }
 
     #Getting json response from OWM
-    response = requests.get('http://api.openweathermap.org/data/2.5/forecast?id=7778677&APPID=26025aec9fb58721adba291f0f3291f2')
+    response = requests.get(
+        'http://api.openweathermap.org/data/2.5/forecast?id=7778677&mode=json&units=metric&APPID=26025aec9fb58721adba291f0f3291f2')
 
     cnx = mysql.connector.connect(**config) # ------------------------- Connecting to RDS
     cursor = cnx.cursor()                   # ------------------------- Cursor to execute all the queries
     weathers = json.loads(response.text)    # ------------------------- Loading JSON Data
 
     # Insert statement to insert values for dublin bike database in the schema ---- %s is the placeholder for the values
-    
-
+    add_weathers = ("INSERT INTO openweathermap_dublin_bikes_dump "
+                   "(weather_id, weather_date, weather_main_temp, weather_main_temp_min, weather_main_temp_max,\
+                   weather_main_pressure, weather_main_sea_level,weather_main_grnd_level, weather_main_humidity,weather_main_temp_kf, weather_weather_id,\
+                    weather_weather_main, weather_weather_des, weather_clouds_all, weather_wind_speed, weather_wind_deg, weather_snow,\
+                     weather_rain, weather_sys_pod, weather_sys_dt_txt) "
+                   "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s,%s,%s,%s,%s,%s,%s,%s)")
     # Generating the key for the database
     max_key = ("SELECT max(weather_id) from openweathermap_dublin_bikes_dump")
     cursor.execute(max_key) # ---------------------- Execute the query on the database
@@ -38,7 +43,7 @@ def main():
         key = 1     # ------------------------------ What if there is no data in the table
     else:
         key = key+1  # ------------------------------ setting the new key
-
+    # print(key)
     for weather in weathers['list']:
         weather_date = time.strftime(
             "%Y-%m-%d %H:%M:%S", time.gmtime(weather['dt'] / 1000.0))
@@ -47,6 +52,7 @@ def main():
         weather_main_temp_max = weather['main']['temp_max']
         weather_main_pressure = weather['main']['pressure']
         weather_main_sea_level = weather['main']['sea_level']
+        weather_main_grnd_level = weather['main']['grnd_level']
         weather_main_humidity = weather['main']['humidity']
         weather_main_temp_kf = weather['main']['temp_kf']
         weather_weather_id = weather['weather'][0]['id']
@@ -59,23 +65,29 @@ def main():
             if '3h' in weather['rain']:
                 weather_rain = weather['rain']['3h']
             else:
-                weather_rain = None
+                weather_rain = 0
         else:
-            weather_rain = None
+            weather_rain = 0
         if 'snow' in weather:
             if '3h' in weather['snow']:
                 weather_snow = weather['snow']['3h']
             else:
-                weather_snow = None
+                weather_snow = 0
         else:
-            weather_snow = None
+            weather_snow = 0
         weather_sys_pod = weather['sys']['pod']
         weather_sys_dt_txt = weather['dt_txt']
 
-        data_station = (key, weather_date, weather_main_temp, weather_main_temp_min, weather_main_temp_max, weather_main_pressure, weather_main_sea_level, weather_main_humidity,
+        data_weathers = (key, weather_date, weather_main_temp, weather_main_temp_min, weather_main_temp_max, weather_main_pressure, weather_main_sea_level,weather_main_grnd_level, weather_main_humidity,
                         weather_main_temp_kf, weather_weather_id, weather_weather_main, weather_weather_des, weather_clouds_all, weather_wind_speed, weather_wind_deg, weather_snow, weather_rain, weather_sys_pod, weather_sys_dt_txt)
-        
+        # print("Inserting: ",data_weathers)
+        cursor.execute(add_weathers,data_weathers)
         key += 1
+    cnx.commit()
+    cnx.close()
+
+if __name__=="__main__":
+    main()
 
     
 
