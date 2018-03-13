9592ed3117b72c89acf7cec62263aef355db001d
Harsh Gupta <harshrp94@gmail.com>
2018-03-13 14:39:01 +0000

    Create Scrapper for JCD

---
+++ dublinbikes/jcd_scrapper.py
@@ -0,0 +1,13 @@
+# Downloads data from JCDeacaux and loads it into Mysql database
+
+import mysql.connector
+import requests
+import json
+import time
+
+def main():
+    pass
+
+
+if __name__=="__main__":
+    main()
