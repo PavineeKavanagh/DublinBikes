3130e831064da6c0a74880882b3ef2061021ebc0
Harsh Gupta <harshrp94@gmail.com>
2018-03-22 20:28:17 +0000

    Adding Info windows to the markers

--- dublinbikes/static/javascript.js
+++ dublinbikes/static/javascript.js
@@ -10,39 +10,58 @@ function initMap() {
     // Changing the icons for the markers
     //Marker for each one
     var markers = [];
+    var infowindows = [];
+    var infowindowcontent = [];
+    for (var i = 0; i < locations.length; i++) {
+        var contentString = '<div id="content">' +
+            '<div id = "content-station" >' + locations[i].name +
+            '</div ><div class=content-numbers>'+
+            '<div class="column"> Bikes' + locations[i].availBikes +
+            '</div>' + '<div class="column">' + locations[i].availStands +
+            '</div>' + '</div>' + '<div id="content-lud">LUD</div>' +
+            '</div >' + '</div>';
+        infowindowcontent.push(contentString);
+    }
+
+    var infowindow = new google.maps.InfoWindow();
     for (var i = 0; i < locations.length; i++) {
         var bikePercent = (locations[i].availBikes / locations[i].tStands) * 100; // --------- Calculating the percentage of number of bikes in each stand
+        var iconbase = '';
+        var latLng = {};
+        titleVal = '';
         if (bikePercent <= 0.25){
             // console.log("Less than 25%"+i);
             // RED MARKER
-            var marker = new google.maps.Marker({
-                position: new google.maps.LatLng(locations[i].lat, locations[i].lng),
-                icon: 'static/img/marker_red.png',
-                map: map,
-                title:locations[i].name
-            });   
-            // markers.push(marker);
+            iconbase = 'static/img/marker_red.png';
+            latLng = { lat: locations[i].lat, lng: locations[i].lng};
+            titleVal = locations[i].name;
         } else if (bikePercent > 0.25 && bikePercent < 0.75){
             // console.log("Greater than 25%"+i);
             // ORANGE MARKER
-            var marker = new google.maps.Marker({
-                position: new google.maps.LatLng(locations[i].lat, locations[i].lng),
-                icon: 'static/img/marker_orange.png',
-                map: map,
-                title:locations[i].name
-            });   
-            // markers.push(marker);
+            iconbase = 'static/img/marker_orange.png';
+            latLng = { lat: locations[i].lat, lng: locations[i].lng };
+            titleVal = locations[i].name;
         } else {
             // console.log("Greater than 75%"+i);
             // GREEN MARKER
-            var marker = new google.maps.Marker({
-                position: new google.maps.LatLng(locations[i].lat, locations[i].lng),
-                icon: 'static/img/marker_green.png',
-                map: map,
-                title:locations[i].name
-            });   
-            // markers.push(marker);
+            iconbase = 'static/img/marker_green.png';
+            latLng = { lat: locations[i].lat, lng: locations[i].lng };
+            titleVal = locations[i].name;
         }
-             
-    }
+        var marker = new google.maps.Marker({
+            position: latLng,
+            icon: iconbase,
+            title: titleVal
+        });  
+        markers.push(marker);
+        // The below code is referred from stack overflow
+        google.maps.event.addListener(marker,'click',(function(marker,i){
+            return function() {
+                infowindow.setContent(infowindowcontent[i]);
+                infowindow.open(map,marker);
+            }
+            })(marker, i));
+        }
+    var markerCluster = new MarkerClusterer(map, markers,
+        { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
